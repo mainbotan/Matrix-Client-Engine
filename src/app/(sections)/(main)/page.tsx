6773835352b3ff6f500a21c26f37f81dc3b1526b
mainbotan <ned.serafim@gmail.com>
2025-11-13 04:03:11 +0300

    topics card

--- src/app/(sections)/(main)/page.tsx
+++ src/app/(sections)/(main)/page.tsx
@@ -1,9 +1,86 @@
 'use client';
 
+import Arrow from '@/assets/ui-kit/icons/arrow';
+import styles from './page.module.scss';
+import Container from "@/app/components/container/container";
+import clsx from 'clsx';
+
 export default function Page() {
     return (
         <>
-            
+            <div className={styles.container}>
+                <div className={styles.focus}>
+                    <div className={styles.title}>Приятной работы, <span className={styles.secondary}>Игорь</span></div>
+                    <div className={styles.topics}>
+                        <div className={styles.capture}>Часто используете</div>
+                        <div className={styles.items}>
+                                <div className={clsx(styles.topic, styles.contrast)}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                                <div className={styles.topic}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                                <div className={styles.topic}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                                <div className={styles.topic}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                                <div className={styles.topic}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                                <div className={styles.topic}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                                <div className={styles.topic}>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Клиенты</div>
+                                        <div className={styles._meta}>Смотрите текущее состояние клиентской базы.</div>
+                                    </div>
+                                    <div className={styles.actions}>
+                                        <div className={styles.action}><Arrow className={styles.svg} /></div>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
