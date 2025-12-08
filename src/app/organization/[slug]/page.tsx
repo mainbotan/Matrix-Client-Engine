9df3b79cfdd11e2f482ba245cf48d617d67345d3
mainbotan <ned.serafim@gmail.com>
2025-12-08 14:39:51 +0300

    refactor 1x

--- src/app/organization/[slug]/page.tsx
+++ src/app/organization/[slug]/page.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.scss';
 import Clients from '@/assets/ui-kit/icons/clients';
 import Kanban from '@/assets/ui-kit/icons/kanban';
 import Plus from '@/assets/ui-kit/icons/plus';
+import Source from '@/assets/ui-kit/icons/source';
 
 export default function Page() {
     return (
@@ -47,7 +48,63 @@ export default function Page() {
                                 <span className={styles.name}>Ожидают</span>
                             </div>
                             <div className={styles.add}><Plus className={styles.svg} /></div>
-                            <div className={styles.block}></div>
+                            <div className={styles.block}>
+                                <div className={styles.start}>
+                                    <div className={styles.icon}><span className={styles.circle} /></div>
+                                    <div className={styles.info}>
+                                        <div className={styles.name}>Client X Y Z</div>
+                                        <div className={styles.description}>Создано 08.12.2025</div>
+                                    </div>
+                                </div>
+                                <div className={styles.center}>
+                                    <div className={styles.comment}>
+                                        Модальных окошек? Траверса к царге на футорку...
+                                        Вопрос НЕ риторический. Вложены не малые деньги для вывода компании на рынок, информацию люди ищут на сайте, уже идут обращения! Встречают по одёжке , а у нас всё криво. Задача сайта рассказать о производстве и товарах и вызвать желание купить!
+                                        Как мы можем говорить о серьезности нашего предприятия если вид настроить не можем. 
+                                        Работы по 1С не видны и совсем не волнуют посетителей, а их не мало, я не случайно спрашивал про счётчик.
+
+                                        <div className={styles.by}>Добавлено <span className={styles.name}>ned.serafim@gmail.com</span></div>
+                                    </div>
+                                </div>
+                                <div className={styles.sections}>
+                                    <section className={styles.section}>
+                                        <div className={styles.icon}><Source className={styles.svg} /></div>
+                                        <div className={styles.info}>
+                                            <div className={styles.capture}>Источник трафика</div>
+                                            <div className={styles.text}>
+                                                Авито: <span className={styles.name}>Главный аккаунт</span>
+                                            </div>
+                                        </div>
+                                    </section>
+                                    <section className={styles.section}>
+                                        <div className={styles.icon}><div style={{backgroundImage: 'url(https://github.githubassets.com/assets/pull-shark-default-498c279a747d.png)'}} className={styles.avatar} /></div>
+                                        <div className={styles.info}>
+                                            <div className={styles.capture}>Возможная выручка</div>
+                                            <div className={styles.count}>
+                                                <span className={styles.primary}>45 000</span>
+                                                -
+                                                <span className={styles.primary}>52 000</span>
+                                                &#8381;
+                                            </div>
+                                        </div>
+                                    </section>
+                                    <section className={styles.section}>
+                                        <div className={styles.icon}><div style={{backgroundImage: 'url(https://github.githubassets.com/assets/pull-shark-default-498c279a747d.png)'}} className={styles.avatar} /></div>
+                                        <div className={styles.info}>
+                                            <div className={styles.capture}>Serafim</div>
+                                            <div className={styles.text}>
+                                                ned.serafim@gmail.com
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
+                                <div className={styles.flags}>
+                                    <span className={clsx(styles.flag, styles.normal)}>Ожидает</span>
+                                    <span className={clsx(styles.flag, styles.critical)}>Критично</span>
+                                    <span className={clsx(styles.flag, styles.problem)}>Проблема</span>
+                                    <span className={clsx(styles.flag, styles.good)}>Хорошо</span>
+                                </div>
+                            </div>
                             <div className={styles.block}></div>
                         </div>
                         <div className={styles.col}>
